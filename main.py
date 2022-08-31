026baad0717d1c13a92b11af21ee04e831894534
Boyd-Randall <111801637+Boyd-Randall@users.noreply.github.com>
2022-08-31 12:33:47 -0600

    Got the 'on shake' function working properly.

--- main.py
+++ main.py
@@ -75,58 +75,33 @@ input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
 })
 input.onGesture(Gesture.Shake, function () {
-    earthquaking = true
-    strip.clear()
-    strip.show()
     music.playSoundEffect(music.createSoundEffect(
-    WaveShape.Sine,
-    912,
-    717,
+    WaveShape.Triangle,
+    74,
+    1,
     255,
-    255,
+    78,
     5000,
-    SoundExpressionEffect.Warble,
-    InterpolationCurve.Curve
-    ), SoundExpressionPlayMode.InBackground)
-    for (let value of list) {
-        strip.setPixelColor(value, neopixel.colors(NeoPixelColors.Red))
+    SoundExpressionEffect.None,
+    InterpolationCurve.Linear
+    ), SoundExpressionPlayMode.UntilDone)
+    for (let value of Strip_length) {
+        if (flipper) {
+            strip.setPixelColor(value, neopixel.colors(NeoPixelColors.Red))
+        }
     }
-    for (let index = 0; index < 15; index++) {
-        strip.rotate(1)
-        strip.show()
-        basic.pause(150)
-        strip.rotate(-1)
-        strip.show()
-        basic.pause(150)
-    }
-    strip.clear()
-    strip.show()
-    earthquaking = false
 })
-let list: number[] = []
+let Strip_length: number[] = []
+let flipper = false
 let strip: neopixel.Strip = null
-let earthquaking = false
-music.setVolume(255)
-earthquaking = false
-strip = neopixel.create(DigitalPin.P0, 15, NeoPixelMode.RGB)
-list = [
-0,
-2,
-4,
-6,
-8,
-10,
-12,
-14
-]
-let flipper = true
+strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
+flipper = true
+Strip_length = [strip.length()]
 strip.setBrightness(150)
 loops.everyInterval(250, function () {
-    if (!(earthquaking)) {
-        if (input.logoIsPressed()) {
-            strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
-        }
-        strip.shift(1)
-        strip.show()
+    if (input.logoIsPressed()) {
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
     }
+    strip.shift(1)
+    strip.show()
 })
